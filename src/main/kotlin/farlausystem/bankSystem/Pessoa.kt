95935d96798a0aac2a01b6e5c1784a13bcaef8ae
Fabiano Alexandre <fabiano.alexandred@gmail.com>
2022-06-27 17:16:41 -0300

    Concept of Inner classes

--- src/main/kotlin/farlausystem/bankSystem/Pessoa.kt
+++ src/main/kotlin/farlausystem/bankSystem/Pessoa.kt
@@ -3,9 +3,15 @@ package farlausystem.bankSystem
 class Pessoa {
     var nome: String = "Fabiano"
     var cpf: String = "123.125.542-78"
+
+    inner class Endereco {
+        var rua: String = "Av. Exemplo de teste"
+        var numero: Int = 781
+    }
 }
 fun main(){
     val fabiano = Pessoa()
 
     println("${fabiano.nome}\n${fabiano.cpf}")
+    println("Rua: ${fabiano.Endereco().rua} ${fabiano.Endereco().numero}")
 }
