98b7392b72e934d18988ed54e332832c426e42be
Fabiano Alexandre <fabiano.alexandred@gmail.com>
2022-06-27 19:00:48 -0300

    Creating the function pessoaInfo and constructor secondary

--- src/main/kotlin/farlausystem/bankSystem/Pessoa.kt
+++ src/main/kotlin/farlausystem/bankSystem/Pessoa.kt
@@ -4,10 +4,14 @@ class Pessoa {
     var nome: String = "Fabiano"
     var cpf: String = "123.125.542-78"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "Nome: $nome \nCPF: $cpf"
 }
 fun main(){
     val fabiano = Pessoa()
 
-    println(fabiano)
-    println("${fabiano.nome}\n${fabiano.cpf}")
+    println(fabiano.pessoaInfo())
+    //println("${fabiano.nome}\n${fabiano.cpf}")
 }
