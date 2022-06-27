ebfc4ab241de47c0c3ccbc5b608177add214d282
Fabiano Alexandre <fabiano.alexandred@gmail.com>
2022-06-27 19:28:06 -0300

    Creating the class account and the functions deposit and withdrawal.

---
+++ src/main/kotlin/farlausystem/bankSystem/account.kt
@@ -0,0 +1,16 @@
+package farlausystem.bankSystem
+
+import java.math.BigDecimal
+
+class account (
+    val ageny: String,
+    val accountNumber: String,
+    val accountBalance: BigDecimal
+){
+    fun deposit(value: BigDecimal){
+
+    }
+    fun withdrawal(value: BigDecimal){
+
+    }
+}
