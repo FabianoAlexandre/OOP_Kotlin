f21d965c4a77e69b22c1e3138debdd7feda0b6f4
Fabiano Alexandre <fabiano.alexandred@gmail.com>
2022-06-27 17:34:33 -0300

    deleting the Inner class and preventing changes to the cpf variable value

--- src/main/kotlin/farlausystem/bankSystem/Pessoa.kt
+++ src/main/kotlin/farlausystem/bankSystem/Pessoa.kt
@@ -3,15 +3,11 @@ package farlausystem.bankSystem
 class Pessoa {
     var nome: String = "Fabiano"
     var cpf: String = "123.125.542-78"
-
-    inner class Endereco {
-        var rua: String = "Av. Exemplo de teste"
-        var numero: Int = 781
-    }
+    private set
 }
 fun main(){
     val fabiano = Pessoa()
 
+    println(fabiano)
     println("${fabiano.nome}\n${fabiano.cpf}")
-    println("Rua: ${fabiano.Endereco().rua} ${fabiano.Endereco().numero}")
 }
